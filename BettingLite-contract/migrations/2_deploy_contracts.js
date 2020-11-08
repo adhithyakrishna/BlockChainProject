3d44c3ae6c725bbceb96bafac26a353c19a628ce
Adhithyakrishna <akovaisr@buffalo.edu>
2020-11-07 19:43:46 -0500

    Updated

--- BettingLite-contract/migrations/2_deploy_contracts.js
+++ BettingLite-contract/migrations/2_deploy_contracts.js
@@ -2,5 +2,5 @@ var BettingLite = artifacts.require("BettingLite.sol");
 
 //NumberOfVoters given is 2, but it can be changed to other value and it will still work.
 module.exports = function(deployer){
-	deployer.deploy(BettingLite)
+	deployer.deploy(BettingLite, "0xfaa88b88830698a2f37dd0fa4acbc258e126bc785f1407ba9824f408a905d784")
 };
